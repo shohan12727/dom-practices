f6e83ad9a6700e6e7ba2d1c3d53f4d45d143ae65
shohan12727 <aishohan001@gmail.com>
2025-08-28 01:31:47 +0600

    cashout funtionlity added

--- Payoo Mobile Bank Project/home.js
+++ Payoo Mobile Bank Project/home.js
@@ -22,6 +22,35 @@ document.getElementById('add_money').addEventListener('click',
     }
 )
 
+// cashout funtion 
+
+
+
+document.getElementById('withdraw_button').addEventListener('click', function () {
+
+
+
+    const cashoutAgentNumber = document.getElementById('cashout_agent_number').value;  // value sting a asche 
+    const cashsoutPin = document.getElementById('cashsout_pin').value; // sting a aslo 
+    
+    if(cashoutAgentNumber.length != 11) {
+        alert('invalid number');
+        return;
+    }
+    if(cashsoutPin !== '1234'){
+        alert('enter 1234');
+        return;
+    }
+    
+    const cahsoutAmmount = document.getElementById('cahsout_ammount').value;
+    const availableBalance = document.getElementById('available_balance').innerText;
+    const updateBalance = parseInt(availableBalance) + parseInt(cahsoutAmmount);
+    document.getElementById('available_balance').innerText = updateBalance;
+
+})
+
+
+
 
 // toggling implementation 
 
