7e5fe66c50e6bec36adeb2fc7f250eb0a524509a
shohan12727 <aishohan001@gmail.com>
2025-08-27 21:25:22 +0600

    add money ui done and index page ui and functionlity done

--- Payoo Mobile Bank Project/script.js
+++ Payoo Mobile Bank Project/script.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+document.getElementById('login_button').addEventListener('click',
+    function (e) {
+        e.preventDefault();
+        const mobileNumberField = document.getElementById('mobile_number');
+        const pinNumberField = document.getElementById('pin_number');
+        const mobileNumber = document.getElementById('mobile_number').value;
+        const pinNumber = document.getElementById('pin_number').value;
+        //  console.log(mobileNumber, pinNumber);
+        if (parseInt(mobileNumber.length) === 11 && parseInt(pinNumber) === 1234) {
+            window.location.href = './home.html';
+        } else {
+            alert('Invalid input')
+        }
+        mobileNumberField.value = '';
+        pinNumberField.value = '';
+    }
+)
