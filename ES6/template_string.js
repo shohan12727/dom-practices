93a31846c94b6de31d5ace44a3dfd523bb9a3a3d
shohan12727 <aishohan001@gmail.com>
2025-08-30 10:39:00 +0600

    template string

---
+++ ES6/template_string.js
@@ -0,0 +1,24 @@
+const poem = `am pata jora jora marbo cabuk chorbo ghora
+ore bubu u=gure dara
+`;
+// console.log(poem);
+
+
+
+const num1 = 10;
+const num2 =20;
+const result = num1 + num2;
+
+
+function sum (num1, num2){
+    const result = num1 + num2;
+    const output = 'sum of ' + num1 + ' and ' + num2 + ' is equal to ' + result;
+    const output2 = `sum of ${num1} and ${num2}  is ${result}`
+    console.log(output,output2);
+}
+
+sum(50,30)
+
+const price = 80;
+const disPrice = `discount price is ${price*0.8}`;
+console.log(disPrice);
