2972731a365c0997587c33338384ed856c949472
shohan12727 <aishohan001@gmail.com>
2025-08-30 10:09:13 +0600

    var const let difference

---
+++ ES6/index.js
@@ -0,0 +1,17 @@
+// let const 
+const name = 'bangladesh';
+const countryName = 'BD' + name;
+console.log(countryName);
+// console.log(price);
+
+let price = 400;
+price = 500;
+console.log(price);
+console.log(device);
+
+var device = 'dell';
+
+
+const dress = ['shirt','pant'];
+dress.push('maflar');
+console.log(dress);
