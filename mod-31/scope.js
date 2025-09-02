ab6e068f74d56c156c0393028c4262d275b93bff
shohan12727 <aishohan001@gmail.com>
2025-09-02 17:31:53 +0600

    scope

---
+++ mod-31/scope.js
@@ -0,0 +1,34 @@
+if (true) {
+    const data = 58;
+    console.log(data);
+    console.log(doMath(20, 40)
+    );
+
+}
+
+const name = 'Mujahid';
+
+// while(true){
+
+// }
+
+
+for (const num of [1, 23, 4, 5, 6, 78]) {
+    console.log(num);
+    console.log(name);
+}
+
+// fucntion scope or local scope 
+
+function doMath(a, b) {
+    console.log(a, b);
+    const sum = a + b;
+    const total = sum + 10;
+    return total
+}
+
+
+
+// function er baire holo global scope
+
+
